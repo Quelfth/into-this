1f9fc48b2de064a5defcce8f80dc9a4e6a13d606
Quelfth <nqfrankl@ucsd.edu>
2025-06-14 17:40:45 -0700

    added try_into support

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@ pub trait IntoThis {
     fn into_this<T>(self) -> T where Self: Into<T> {
         self.into()
     }
+    
+    fn try_into_this<T>(self) -> Result<T, Self::Error> where Self: TryInto<T> {
+        self.try_into()
+    }
 }
 
 impl<T> IntoThis for T { }
